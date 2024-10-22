0778d395e6d12cc52496129696e5cc28cdb3a5da
curiopasect <curiopasect@gmail.com>
2024-10-22 12:28:36 +0330

    Update migration.sh

--- migration.sh
+++ migration.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
+# Define color codes
+GREEN="\033[32m"
+RESET="\033[0m"
 
 # Step 1: Updating package list
-echo "Updating package list and installing Nginx..."
+echo -e "${GREEN}Updating package list and installing Nginx...${RESET}"
 sudo apt update
-
 
 # Step 2: Copy directories from Server 1 to Server 2
 directories=("/etc/letsencrypt" "/etc/x-ui-english")
 username="root"
 read -p "Enter the IP address of Server 1: " server_ip
 for dir in "${directories[@]}"; do
-  echo "Copying $dir from $username@$server_ip to $dir on Server 2"
+  echo -e "${GREEN}Copying $dir from $username@$server_ip to $dir on Server 2${RESET}"
   sudo scp -r "$username@$server_ip:$dir/" "$dir"
 done
 
-
 # Step 3: Install x-ui-english on Server 2
-echo "Installing x-ui-english..."
+echo -e "${GREEN}Installing x-ui-english...${RESET}"
 bash <(curl -Ls https://raw.githubusercontent.com/NidukaAkalanka/x-ui-english/master/install.sh)
 x-ui restart
 
-
-
-echo "All directories have been copied to their original locations on Server 2!"
+echo -e "${GREEN}Migration successful.${RESET}"
