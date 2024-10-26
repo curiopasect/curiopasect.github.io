17776ef00e4ea40fc8874731cb9f8350d1b020d2
curiopasect <curiopasect@gmail.com>
2024-10-26 20:32:35 +0330

    Update migration.sh

--- migration.sh
+++ migration.sh
@@ -7,7 +7,7 @@ RESET="\033[0m"
 
 clear
 echo "Starting the process..."
-sleep 3
+sleep 1.5
 echo "Process has started."
 
 # Step 1: Updating package list
@@ -15,7 +15,7 @@ echo -e "${GREEN}Updating package list...${RESET}"
 sudo apt update
 echo -e "${GREEN}Update successful.${RESET}"
 sleep 1.5
-clear
+echo -e "\n\n\n\n\n\n\n"
 
 # Step 2: Copy directories from Server 1 to Server 2
 username="root"
@@ -23,17 +23,16 @@ read -p "Enter the IP address of the origin server: " server_ip
 echo -e "${GREEN}Retrieving directories from $username@$server_ip...${RESET}"
 sleep 1
 sudo scp -r "root@$server_ip:/etc/letsencrypt/" "/etc/letsencrypt" && scp -r "root@$server_ip:/etc/x-ui-english/" "/etc/x-ui-english"
-echo -e "${GREEN}Retrieve successful.${RESET}"
-sleep 1.5
-clear
+sleep 2.5
+echo -e "\n\n\n\n\n\n\n"
 
 # Step 3: Install x-ui-english on Server 2
 echo -e "${GREEN}Installing x-ui-english...${RESET}"
 bash <(curl -Ls https://raw.githubusercontent.com/NidukaAkalanka/x-ui-english/master/install.sh)
 x-ui restart
-clear
+echo -e "\n\n\n\n\n\n\n"
 
 # Result
-echo -e "${GREEN}Migration successful.${RESET}"
+echo -e "${GREEN}Migration attempted and probably succeeded.${RESET}"
 sleep 2
-echo -e "${YELLOW}Next steps:\n\t- Fetch new v2ray configs from this server's X-UI Panel.\n\t- Add/replace them in https://github.com/curiopasect/kiwi/edit/main/index.html.${RESET}"
+echo -e "${YELLOW}Next steps:\n\t- Fetch new v2ray configs from this very server's X-UI Panel.\n\t- Add/replace them in https://github.com/curiopasect/kiwi/edit/main/index.html.${RESET}"
